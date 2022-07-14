f5451dd1405bf6f616c8b2fab81b759da3e36ac4
Karn <karnapper@gmail.com>
2022-07-14 16:50:45 +0700

    update: add index route

--- src/server.ts
+++ src/server.ts
@@ -7,35 +7,37 @@ import express, {
 } from "express";
 import { Server } from "http";
 import createHttpError from "http-errors";
-
+import expressConfig from "./configs/express";
+import route from "./routes";
 const app: Application = express();
 const config: any = "./configs/app";
-// app.use("./configs/express");
-// app.use("./routes");
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello from ts server");
-});
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new createHttpError.NotFound());
-});
-
-const errorHandler: ErrorRequestHandler = (
-  err,
-  req: Request,
-  res: Response
-) => {
-  res.status(err.status || 500);
-  res.send({
-    status: err.status || 500,
-    message: err.message,
-  });
-};
-
-app.use(errorHandler);
+expressConfig(app);
+app.use(route);
 
 const PORT: number = config.PORT || 1337;
 const server: Server = app.listen(PORT, () => {
   console.log(`Server is on Port ${PORT}`);
 });
+
+// app.get("/", (req: Request, res: Response, next: NextFunction) => {
+//   res.send("Hello from ts server");
+// });
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   next(new createHttpError.NotFound());
+// });
+
+// const errorHandler: ErrorRequestHandler = (
+//   err,
+//   req: Request,
+//   res: Response
+// ) => {
+//   res.status(err.status || 500);
+//   res.send({
+//     status: err.status || 500,
+//     message: err.message,
+//   });
+// };
+
+// app.use(errorHandler);
