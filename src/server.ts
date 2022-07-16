b983d7d2b25ba26ad3f7078e54b24a8256a51d47
Karn-P <karnapper@gmail.com>
2022-07-16 10:57:28 +0700

    update: remove error

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-import express, {
-  Application,
-  ErrorRequestHandler,
-  NextFunction,
-  Request,
-  Response,
-} from "express";
+import express, { Application } from "express";
 import { Server } from "http";
-import createHttpError from "http-errors";
 import expressConfig from "./configs/express";
 import route from "./routes";
 const app: Application = express();
@@ -19,25 +12,3 @@ const PORT: number = config.PORT || 3001;
 const server: Server = app.listen(PORT, () => {
   console.log(`Server is on Port ${PORT}`);
 });
-
-// app.get("/", (req: Request, res: Response, next: NextFunction) => {
-//   res.send("Hello from ts server");
-// });
-
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   next(new createHttpError.NotFound());
-// });
-
-// const errorHandler: ErrorRequestHandler = (
-//   err,
-//   req: Request,
-//   res: Response
-// ) => {
-//   res.status(err.status || 500);
-//   res.send({
-//     status: err.status || 500,
-//     message: err.message,
-//   });
-// };
-
-// app.use(errorHandler);
